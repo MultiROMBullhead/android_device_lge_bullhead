8641e5d3fb38deafbeab88e80c27958773c0b648
AdamsKen <adams.ken@protonmail.com>
2016-09-13 14:36:10 +0530

    Created mr_qcom_overlay.h

---
+++ mr_qcom_overlay.h
@@ -0,0 +1,9 @@
+#FROM https://github.com/multirom-nexus6p/TWRP-android_device_huawei_angler/blob/android-6.0-mrom-notheme/multirom/mr_qcom_overlay.h
+
+#ifndef MR_QCOM_OVERLAY_H
+#define MR_QCOM_OVERLAY_H
+
+#include "msm_ion.h"
+#include "msm_mdp.h"
+
+#endif
